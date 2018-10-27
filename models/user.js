67a64e719828a8f22415faa94c3385a02f4c3b73
Elia Neaimeh <elianeaimeh@Elias-MacBook-Pro.local>
2018-10-28 01:11:20 +0400

    first push request for the NodeJS project

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 5,
+    maxlength: 50,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  }
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  return token;
+}
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  };
+  return Joi.validate(user, schema);
+};
+
+
+exports.User = User;
+exports.Validate = validateUser;
